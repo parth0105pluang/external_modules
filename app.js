87f49b5e5093521ea61774be4430fc89f2ff7fc3
Parth Sharma <parthsharma@Parths-MacBook-Pro.local>
2022-01-22 20:43:30 +0530

    scripts:start

---
+++ app.js
@@ -0,0 +1,8 @@
+// local dependency
+//npm i <packageName>
+// global dependency
+// sudo npm install -g <pkg>
+//npm init->pkg.json
+//npm init -y(every thing default
+// devDependencies nodemon only during development
+console.log('parth')
